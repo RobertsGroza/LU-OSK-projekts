4d44cc58a7e5e99104767b0dcae13d7e2ff8b7ce
RobertsGroza <roberts.groza23@gmail.com>
2019-04-08 11:49:10 +0300

    Add SSTF, SCAN & C-SCAN algorithms

--- src/js/functions.js
+++ src/js/functions.js
@@ -24,32 +24,38 @@ function clearForm() {
 function visualize() {
   // Find elements
   let queue = document.getElementById('queue');
-  let startPosition = document.getElementById('headStartPosition');
+  let startPosition = parseInt(document.getElementById('headStartPosition').value);
+  let cylinderCount = parseInt(document.getElementById('cylinderCount').value);
   let algorithm = document.getElementById('algorithm');
   let startRightCheckbox = document.getElementById('startRight');
   let startLeftCheckbox = document.getElementById('startLeft');
   let resultDiv = document.getElementById('result');
   let walkedCylinderCounter = document.getElementById('numberOfWalkedCilinders');
   let cylinderSequence = document.getElementById('cylinderSequence');
+
+  // Get cylinder array
   let cylinderArray = queue.value.replace(/\s/g, "").split(",").map(v => parseInt(v)); // get queue as integer array
-  cylinderArray = [98, 183, 37, 122, 14, 124, 65, 67];
   let resultArray;
+
+  cylinderArray = [98, 183, 37, 122, 14, 124, 65, 67];
+  startPosition = 53;
+  cylinderCount = 200;
 
   console.log('before: ', cylinderArray);
 
   // Depending on input values determines which function is called
   switch (algorithm.options[algorithm.selectedIndex].value) {
     case 'FCFS':
-      resultArray = FCFS(parseInt(startPosition.value), cylinderArray);
+      resultArray = FCFS(startPosition, cylinderArray);
       break;
     case 'SSTF':
-      SSTF();
+      resultArray = SSTF(startPosition, cylinderArray);
       break;
     case 'SCAN':
-      startRightCheckbox.checked ? SCANLeftToRight() : SCANRightToLeft();
+      resultArray = startRightCheckbox.checked ? SCANLeftToRight(startPosition, cylinderArray, cylinderCount) : SCANRightToLeft(startPosition, cylinderArray);
       break;
     case 'C-SCAN':
-      startRightCheckbox.checked ? CSCANLeftToRight() : CSCANRightToLeft();
+      resultArray = startRightCheckbox.checked ? CSCANLeftToRight(startPosition, cylinderArray, cylinderCount) : CSCANRightToLeft(startPosition, cylinderArray, cylinderCount);
       break;
     case 'C-LOOK':
       startRightCheckbox.checked ? CLOOKLeftToRight() : CLOOKRightToLeft();
@@ -91,43 +97,126 @@ function SSTF(startingPosition, cylinderArray) {
   let usedArrayIndexes = [];
   resultArray[0] = startingPosition;
 
-  cylinderArray.map(function(el, idx, array){
-    array.map(function (e, i, arr) {
-      // Find closest element to current element (which is last in the result array)
-      let closestCylinderIndex;
-      let closestDifference;
+  for (let i = 0; i < cylinderArray.length; i++) {
+    // atrod tuvākā elementa indexu rezultāta masīva pēdējam elementam, turklāt indekss nevar atrastes izmantoto indexu masīvā
+    let smallestDiffIndex = -1; // Norāda, ka otrais for cikls tikko sākās
+    let smallestDiff;
 
-      if (!usedArrayIndexes.includes(i)) {
-        closestCylinderIndex = i;
-        closestDifference = Math.abs(arr[idx] - resultArray[resultArray.length - 1]);
+    for (let j = 0; j < cylinderArray.length; j++) {
+      // Pieņem, ka pirmais elements masīvā, kas nav izmantots ir ar vismazāko starpību
+      if (smallestDiffIndex === -1 && !usedArrayIndexes.includes(j)) {
+        smallestDiff = Math.abs(resultArray[resultArray.length - 1] - cylinderArray[j]);
+        smallestDiffIndex = j;
+        continue;
       }
 
-    })
-  });
+      // Ja tas nav pirmais elements, kas nav jau iekļauts rezultāta sarakstā tad pārbauda, vai starpība nav mazāka
+      let diff = Math.abs(resultArray[resultArray.length - 1] - cylinderArray[j]);
+      if (smallestDiffIndex !== -1 && !usedArrayIndexes.includes(j) && diff < smallestDiff) {
+        smallestDiff = diff;
+        smallestDiffIndex = j;
+      }
+    }
+
+    // rezultātu masīvam pievieno atrasto elementu
+    usedArrayIndexes.push(smallestDiffIndex);
+    resultArray.push(cylinderArray[smallestDiffIndex]);
+  }
+
+  return resultArray;
 }
 
 // SCAN Algorithm realisation and first direction is left to right
-function SCANLeftToRight(startingPosition, cylinderArray) {
-  console.log('scan-left');
+function SCANLeftToRight(startingPosition, cylinderArray, cylinderCount) {
+  let resultArray = [].concat(cylinderArray); // Concat so cylinder array do not get changed in process
+  resultArray.push(startingPosition);
+  resultArray.sort((a, b) => a - b); // Sakārto doto masīvu augošā secībā
+  !resultArray.includes(cylinderCount - 1) && resultArray.push(cylinderCount - 1);  // Pievieno galējo cilindru masīva beigās
+
+  let startingIndex = resultArray.indexOf(startingPosition);
+
+  let resultArray2 = [];
+  for (let i = startingIndex; i < resultArray.length; i++) {
+    resultArray2.push(resultArray[i]);
+  }
+
+  for (let i = startingIndex - 1; i >= 0; i--) {
+    resultArray2.push(resultArray[i]);
+  }
+  return resultArray2;
 }
 
 // SCAN Algorithm realisation and first direction is right to left
 function SCANRightToLeft(startingPosition, cylinderArray) {
-  console.log('scan-right');
+  let resultArray = [].concat(cylinderArray); // Concat so cylinder array do not get changed in process
+  resultArray.push(startingPosition);
+  resultArray.sort((a, b) => b - a); // Sakārto doto masīvu distošā secībā
+  !resultArray.includes(0) && resultArray.push(0);  // pievieno mazāko cilindru masīva beigās
+
+  let startingIndex = resultArray.indexOf(startingPosition);
+
+  let resultArray2 = [];
+  for (let i = startingIndex; i < resultArray.length; i++) {
+    resultArray2.push(resultArray[i]);
+  }
+
+  for (let i = startingIndex - 1; i >= 0; i--) {
+    resultArray2.push(resultArray[i]);
+  }
+
+  return resultArray2;
 }
 
 // CSCAN Algorithm realisation and first direction is left to right
-function CSCANLeftToRight(startingPosition, cylinderArray) {
-  console.log('cscan-left');
+function CSCANLeftToRight(startingPosition, cylinderArray, cylinderCount) {
+  let resultArray = [].concat(cylinderArray); // Concat so cylinder array do not get changed in process
+  resultArray.push(startingPosition);
+  resultArray.sort((a, b) => a - b); // Sakārto doto masīvu augošā secībā
+  !resultArray.includes(0) && resultArray.unshift(0);  // Pievieno pirmo cilindru masīva sākumā
+  !resultArray.includes(cylinderCount - 1) && resultArray.push(cylinderCount - 1);  // Pievieno galējo cilindru masīva beigās
+
+  let startingIndex = resultArray.indexOf(startingPosition);
+  let resultArray2 = [];
+
+  for (let i = startingIndex; i < resultArray.length; i++) {
+    resultArray2.push(resultArray[i]);
+  }
+
+  if (startingIndex !== 0) {
+    for (let i = 0; i < startingIndex; i++) {
+      resultArray2.push(resultArray[i]);
+    }
+  }
+
+  return resultArray2;
 }
 
 // CSCAN Algorithm realisation and first direction is right to left
-function CSCANRightToLeft(startingPosition, cylinderArray) {
-  console.log('cscan-right');
+function CSCANRightToLeft(startingPosition, cylinderArray, cylinderCount) {
+  let resultArray = [].concat(cylinderArray); // Concat so cylinder array do not get changed in process
+  resultArray.push(startingPosition);
+  resultArray.sort((a, b) => b - a); // Sakārto doto masīvu distošā secībā
+  !resultArray.includes(0) && resultArray.push(0);  // Pievieno pirmo cilindru masīva sākumā
+  !resultArray.includes(cylinderCount - 1) && resultArray.unshift(cylinderCount - 1);  // Pievieno galējo cilindru masīva beigās
+
+  let startingIndex = resultArray.indexOf(startingPosition);
+  let resultArray2 = [];
+
+  for (let i = startingIndex; i < resultArray.length; i++) {
+    resultArray2.push(resultArray[i]);
+  }
+
+  if (startingIndex !== resultArray.length) {
+    for (let i = 0; i < startingIndex; i++) {
+      resultArray2.push(resultArray[i]);
+    }
+  }
+
+  return resultArray2;
 }
 
 // CLOOK Algorithm realisation and first direction is left to right
-function CLOOKLeftToRight(startingPosition, cylinderArray) {
+function CLOOKLeftToRight(startingPosition, cylinderArray, cylinderCount) {
   console.log('clook-left');
 }
 
